25c722597f985d746c1027e217100801dbb341ff
Felix Schmidt <s224313@dtu.dk>
2023-04-12 17:54:59 +0200

    JavaDoc for all packages

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * dk.dtu.compute.se.pisd.roborally
+ * contains the roborally controller-, model- and view package.
+ * It has the LICENSE and overview as well.
+ */
+package dk.dtu.compute.se.pisd.roborally;
+
+
