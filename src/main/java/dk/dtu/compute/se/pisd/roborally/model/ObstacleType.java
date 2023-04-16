789091618bad9165b62e947a1b04fcfdc3f3c4e0
ZEE <Zaidsh02@gmail.com>
2023-04-17 01:13:41 +0200

    Updated ObstacleType name

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/ObstacleType.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/ObstacleType.java
@@ -1,9 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 public enum ObstacleType {
-    BLUE_CONVEYOR_BELTS,
-    GREEN_CONVEYOR_BELTS,
-    PUSH_PANELS,
-    GEARS,
+    BLUE_CONVEYOR_BELT,
+    GREEN_CONVEYOR_BELT,
+    PUSH_PANEL,
+    GEAR,
     BOARD_LASER
 }
