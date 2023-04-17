58b6ed49781ea50f85d45f72ecca0a97f2bc2e05
ZEE <Zaidsh02@gmail.com>
2023-04-17 15:00:50 +0200

    ObstacleType constructor

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Obstacle.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Obstacle.java
@@ -1,5 +1,13 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
+import static dk.dtu.compute.se.pisd.roborally.model.Heading.SOUTH;
+
 public class Obstacle {
     private ObstacleType type;
+    private Heading heading = SOUTH;
+
+    public Obstacle(ObstacleType type){
+        this.type = type;
+    }
+
 }
