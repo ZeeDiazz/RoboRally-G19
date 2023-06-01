2630061f5e07d4d566137a292bf38316c9a24dbe
Zigalow <s224302@dtu.dk>
2023-06-01 23:52:45 +0200

    Added to Player, PlayerType and Robot classes

--- src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/PlayerType.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/PlayerType.java
@@ -1,4 +1,14 @@
 package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
 
+import javax.naming.Name;
+
 public abstract class PlayerType {
+
+    private String name;
+    private int playerID;   // playerIndex
+
+    public PlayerType(String name, int playerID) {
+        this.name = name;
+        this.playerID = playerID;
+    }
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/Robot.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/Robot.java
@@ -1,4 +1,27 @@
 package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
 
-public class Robot {
+
+import dk.dtu.compute.se.pisd.roborally.old.model.Player;
+import dk.dtu.compute.se.pisd.roborally.old.model.spaces.Space;
+import dk.dtu.compute.se.pisd.roborally.online.designpatterns.observer.Subject;
+
+public class Robot extends Subject {
+
+    final public static int NUMBER_OF_REGISTERS = 5;
+    final public static int NUMBER_OF_CARDS = 8;
+    public int checkpointReached = 0;
+    private int energyCubes;
+
+    private Space space;
+    private String color;
+
+
+//  private Position rebootPosition;
+//  private Heading heading = Heading.SOUTH;
+
+//    private CommandCardField[] program;
+//    private CommandCardField[] cards;
+//    private Command prevProgramming;
+
+
 }
