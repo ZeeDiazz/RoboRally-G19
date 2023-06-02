15f7482ab58c3e7b6e79cab3437fe928d2c073f6
Zigalow <s224302@dtu.dk>
2023-06-03 00:53:44 +0200

    Added all the view files

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/ui_view/RoboRallyMenuBar.java
@@ -0,0 +1,82 @@
+package dk.dtu.compute.se.pisd.roborally.online.mvc.ui_view;
+import dk.dtu.compute.se.pisd.roborally.online.mvc.client_controller.AppController;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+/**
+ * ...
+ * This is a class for creating menu bars for RoboRally with options for:
+ * new game, save game,load game, stop game and exit
+ * @author Ekkart Kindler, ekki@dtu.dk
+ *
+ */
+public class RoboRallyMenuBar extends MenuBar {
+
+    private AppController appController;
+
+    private Menu controlMenu;
+
+    private MenuItem saveGame;
+
+    private MenuItem newGame;
+
+    private MenuItem loadGame;
+
+    private MenuItem stopGame;
+
+    private MenuItem exitApp;
+
+    /**
+     * This method creates a new menu bar with several options
+     * @param appController Controller of the app
+     */
+    public RoboRallyMenuBar(AppController appController) {
+        this.appController = appController;
+
+        controlMenu = new Menu("File");
+        this.getMenus().add(controlMenu);
+
+        newGame = new MenuItem("New Game");
+        newGame.setOnAction( e -> this.appController.newGame());
+        controlMenu.getItems().add(newGame);
+
+        stopGame = new MenuItem("Stop Game");
+        stopGame.setOnAction( e -> this.appController.stopGame());
+        controlMenu.getItems().add(stopGame);
+
+        saveGame = new MenuItem("Save Game");
+        saveGame.setOnAction( e -> this.appController.saveGame());
+        controlMenu.getItems().add(saveGame);
+
+        loadGame = new MenuItem("Load Game");
+        loadGame.setOnAction( e -> this.appController.loadGame());
+        controlMenu.getItems().add(loadGame);
+
+        exitApp = new MenuItem("Exit");
+        exitApp.setOnAction( e -> this.appController.exit());
+        controlMenu.getItems().add(exitApp);
+
+        controlMenu.setOnShowing(e -> update());
+        controlMenu.setOnShown(e -> this.updateBounds());
+        update();
+    }
+
+    /**
+     * This method updates the view of the menu depending on the stage of the game
+     */
+    public void update() {
+        if (appController.isGameRunning()) {
+            newGame.setVisible(false);
+            stopGame.setVisible(true);
+            saveGame.setVisible(true);
+            loadGame.setVisible(false);
+        } else {
+            newGame.setVisible(true);
+            stopGame.setVisible(false);
+            saveGame.setVisible(false);
+            loadGame.setVisible(true);
+        }
+    }
+
+}
---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/ui_view/SpaceView.java
@@ -0,0 +1,121 @@
+package dk.dtu.compute.se.pisd.roborally.online.mvc.ui_view;
+
+/**
+ * ...
+ * This class creates a black or white space on the game board with a specific height and width
+ * if the space contains a player, a player is displayed as a polygon
+ *
+ * @author Ekkart Kindler, ekki@dtu.dk
+ */
+
+
+import dk.dtu.compute.se.pisd.roborally.online.designpatterns.observer.Subject;
+import dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model.HeadingDirection;
+import dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model.Player;
+import dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model.spaces.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import org.jetbrains.annotations.NotNull;
+public class SpaceView extends StackPane implements ViewObserver {
+
+    final public static int SPACE_HEIGHT = 60; // 60; // 75;
+    final public static int SPACE_WIDTH = 60;  // 60; // 75;
+    final public static double wallThickness = 3;
+    final public static Color wallColor = Color.RED;
+
+    public final Space space;
+
+    /**
+     * This method creates a new singular space
+     *
+     * @param space The space that will be viewed
+     * @author ZeeDiazz (Zaid), Felix723, Zahed Wafa
+     * Assigning different colors to different types of Obstacle
+     */
+    public SpaceView(@NotNull Space space) {
+        this.space = space;
+
+        // XXX the following styling should better be done with styles
+        this.setPrefWidth(SPACE_WIDTH);
+        this.setMinWidth(SPACE_WIDTH);
+        this.setMaxWidth(SPACE_WIDTH);
+
+        this.setPrefHeight(SPACE_HEIGHT);
+        this.setMinHeight(SPACE_HEIGHT);
+        this.setMaxHeight(SPACE_HEIGHT);
+
+        String style;
+        if (space instanceof BlueConveyorSpace) {
+            style = "-fx-background-color: blue;";
+        }
+        else if (space instanceof GreenConveyorSpace) {
+            style = "-fx-background-color: green;";
+        }
+        else if (space instanceof RedGearSpace) {
+            style = "-fx-background-color: red;";
+        }
+        else if (space instanceof GreenGearSpace) {
+            style = "-fx-background-color: darkgreen;";
+        }
+        else if (space instanceof PitSpace) {
+            style = "-fx-background-color: black;";
+        }
+        else if (space instanceof CheckPointSpace) {
+            style = "-fx-background-color: yellow;";
+        }
+        else if ((space.position.X + space.position.Y) % 2 == 0) {
+            style = "-fx-background-color: white;";
+        }
+        else {
+            style = "-fx-background-color: gray;";
+        }
+        this.setStyle(style);
+
+        // Zahed Wafa {
+        double top = space.hasWall(HeadingDirection.NORTH) ? wallThickness : 0;
+        double right = space.hasWall(HeadingDirection.EAST) ? wallThickness : 0;
+        double bottom = space.hasWall(HeadingDirection.SOUTH) ? wallThickness : 0;
+        double left = space.hasWall(HeadingDirection.WEST) ? wallThickness : 0;
+
+        BorderWidths borderWidths = new BorderWidths(top, right, bottom, left);
+        BorderStroke borderStroke = new BorderStroke(wallColor, BorderStrokeStyle.SOLID, null, borderWidths);
+        Border border = new Border(borderStroke);
+        this.setBorder(border);
+        // Zahed Wafa }
+
+        // }
+
+        // updatePlayer();
+
+        // This space view should listen to changes of the space
+        space.attach(this);
+        update(space);
+    }
+
+    private void updatePlayer() {
+        this.getChildren().clear();
+
+        Player player = space.getRobot().getOwner();
+        if (player != null) {
+            Polygon arrow = new Polygon(0.0, 0.0,
+                    10.0, 20.0,
+                    20.0, 0.0);
+            try {
+                arrow.setFill(Color.valueOf(player.robot.getColor()));
+            } catch (Exception e) {
+                arrow.setFill(Color.MEDIUMPURPLE);
+            }
+
+            arrow.setRotate((90 * player.robot.getHeadingDirection().ordinal()) % 360);
+            this.getChildren().add(arrow);
+        }
+    }
+
+    @Override
+    public void updateView(Subject subject) {
+        if (subject == this.space) {
+            updatePlayer();
+        }
+    }
+}
