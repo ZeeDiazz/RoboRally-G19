eb842bff924da1759cbb2e31075c139e21fb8d5c
Zigalow <s224302@dtu.dk>
2023-06-05 02:04:41 +0200

    Made listener interface for when game is finished

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/GameFinishedListener.java
@@ -0,0 +1,11 @@
+package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
+
+/**
+ * Interface listener that waits for event. In this case, it's for when the game is finished
+ *
+ * @author Zigalow
+ */
+public interface GameFinishedListener {
+
+    void onGameFinished();
+}
