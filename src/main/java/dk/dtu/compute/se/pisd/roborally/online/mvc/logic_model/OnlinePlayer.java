e3d7e75fb0388ce1adbb296fd30cad4711e5ebcd
Zigalow <s224302@dtu.dk>
2023-06-06 17:42:13 +0200

    Added OnlinePlayer class. Right now, it works like the LocalPlayer, but it's implemented with the serialization, so that it's ready for implementations

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/OnlinePlayer.java
@@ -0,0 +1,11 @@
+package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
+
+import org.jetbrains.annotations.NotNull;
+
+public class OnlinePlayer extends Player {
+
+    public OnlinePlayer(Game game, String color, @NotNull String name) {
+        super(game, color, name);
+    }
+
+}
