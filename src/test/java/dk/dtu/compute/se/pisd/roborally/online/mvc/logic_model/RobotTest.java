9a11015d936bd1987316edb5b5ab038bac743b06
ZEE <Zaidsh02@gmail.com>
2023-06-11 13:41:22 +0200

    Created RobotTest.java

---
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/RobotTest.java
@@ -0,0 +1,33 @@
+package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RobotTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSpace() {
+    }
+
+    @Test
+    void getHeadingDirection() {
+    }
+
+    @Test
+    void getRebootPosition() {
+    }
+
+    @Test
+    void getColor() {
+    }
+
+    @Test
+    void getOwner() {
+    }
+}
