667b5f343550de569351baa4e9023e4b65b68403
ZEE <Zaidsh02@gmail.com>
2023-06-11 14:10:52 +0200

    Coded Unittest for getSpace()

--- src/test/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/RobotTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/online/mvc/logic_model/RobotTest.java
@@ -1,18 +1,35 @@
 package dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model;
 
+import dk.dtu.compute.se.pisd.roborally.online.mvc.logic_model.spaces.Space;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
+    private Robot robot;
+    private Player owner;
 
     @BeforeEach
     void setUp() {
+        robot = new Robot("Green",owner);
     }
 
     @Test
     void getSpace() {
+        //The robot isn't assigned a Space yet
+        Space startingRobotSpace = robot.getSpace();
+        assertNull(startingRobotSpace);
+
+        //Place the robot at the space 1,0
+        Space space = new Space(new Position(1, 0));
+        robot.setSpace(space);
+
+        //Check if the player is on that space
+        assertEquals(space, robot.getSpace());
+
+        //Check if the player no longer is
+        assertNotEquals(startingRobotSpace,robot.getSpace());
     }
 
     @Test
